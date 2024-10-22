8c6ac3baaae426bcb2fee0f59aa7f1a2805f909f
Zhou_zhou <2746486175@qq.com>
2024-10-22 16:42:33 +0800

    导航功能开通

--- my_robot_description/launch/gazebo_launch.py
+++ my_robot_description/launch/gazebo_launch.py
@@ -1,14 +1,18 @@
 import os
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess,IncludeLaunchDescription, SetEnvironmentVariable
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from launch.actions import TimerAction
 import subprocess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
 
 
 def generate_launch_description():
 
+
+#xacro_file_3V3=os.path.join(get_package_share_directory())
     # 获取文件路径
     xacro_file = os.path.join(
         get_package_share_directory("my_robot_description"),
@@ -37,9 +41,23 @@ def generate_launch_description():
     except subprocess.CalledProcessError as e:
         print(f"Error converting Xacro to URDF: {e}")
         return LaunchDescription([])
+    
+    navigation2_launch_file = os.path.join(
+        get_package_share_directory('turtlebot3_navigation2'),
+        'launch',
+        'navigation2.launch.py'
+    )
+    navigation2_launch = IncludeLaunchDescription(
+                            PythonLaunchDescriptionSource(navigation2_launch_file),
+                            launch_arguments={
+                                'use_sim_time': 'true',
+                                 'map': '/home/zhy/zhy-ros2/my_robot/my_robot_description/maps/7v7map.yaml'
+                            }.items()
+                        )
 
     return LaunchDescription(
         [
+             SetEnvironmentVariable('TURTLEBOT3_MODEL', 'waffle'),
             ExecuteProcess(
                 cmd=[
                     "/usr/bin/gzserver",
@@ -51,6 +69,7 @@ def generate_launch_description():
                 ],
                 output="screen",
             ),
+
             ExecuteProcess(cmd=["/usr/bin/gzclient"], output="screen"),
             TimerAction(
                 period=5.0,  # 等待 5 秒确保 Gazebo 启动
@@ -64,9 +83,36 @@ def generate_launch_description():
                             urdf_file,
                             "-entity",
                             "my_robot",
+                            "-x", "7",  # 设置初始位置的 x 坐标
+                            "-y", "4",  # 设置初始位置的 y 坐标
+                            "-z", "0",  # 设置初始位置的 z 坐标
                         ],
                     ),
+
+                    Node(
+                        package="robot_state_publisher",
+                        executable="robot_state_publisher",
+                        name="robot_state_publisher",
+                        output="screen",
+                        parameters=[
+                            {"robot_description": open(urdf_file).read()}
+                        ],  # 加载 URDF 文件内容
+                    ),
+                    # 包含 `navigation2.launch.py` 文件并传递参数
+                    #   Node(
+                    #         package='nav2_controller',
+                    #         executable='controller_server',
+                    #         parameters=[
+                    #                         PathJoinSubstitution(
+                    #                             [get_package_share_directory('my_robot_description'), 'config', 'navigation.yaml']
+                    #                         )
+                    #                      ],
+                    #         output='screen',
+                    #         ),
+                     navigation2_launch,
                 ],
             ),
+        
+    
         ]
     )
